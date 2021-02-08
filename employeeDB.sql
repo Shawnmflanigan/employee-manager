424ced928f72ac1870916d90baed1580b1986bda
Shawn Flanigan <shawnmflanigan@gmail.com>
2021-02-08 08:56:02 -0800

    add data to employeeDB

--- employeeDB.sql
+++ employeeDB.sql
@@ -30,3 +30,9 @@ CREATE TABLE employee (
 
 INSERT INTO departments (name)
 VALUES ("Sales"), ("Engineering"), ("Legal"), ("Hospitality");
+
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Business Development", 50000.00, 1), ("Sales Engineer", 60000.00, 1), ("Junior Developer", 60000.00, 2), ("Full Stack Developer", 80000.00, 2), ("In House Council", 100000.00, 3), ("Kitchen Staff", 35000.00, 4), ("Operations", 45000.00, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Tom", "Frank", 1, 2), ("Percy", "Waters", 1, 2), ("Jane", "Doe", 2, 4), ("Stacy", "Tavarez", 2, 4), ("Hello", "Dolly", 3, 5), ("Terrance", "Noname", 4, 7), ("That", "Person", 4, 7);
